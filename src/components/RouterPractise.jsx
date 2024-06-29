06bc2f4881b0afcef39d08d8214335c59d340d7a
Hithesh B <hitesh99017@gmail.com>
2024-06-30 00:44:26 +0530

    react router practise

---
+++ src/components/RouterPractise.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Home from './Home'
+import About from './About'
+import Service from './Service'
+import Navbar from './Navbar'
+
+const RouterPractise = () => {
+const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<><Home/></>
+    },
+    {
+        path:'/about',
+        element:<><Navbar/><About/></>
+    },
+    {
+        path:'/service',
+        element:<><Navbar/><Service/></>
+    }
+])
+  return (
+    <div>
+      <RouterProvider router={router}/>
+    </div>
+  )
+}
+
+export default RouterPractise
