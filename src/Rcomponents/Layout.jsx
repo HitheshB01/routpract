bf2e05aa215a6a2a18eb3c61ba16c6a8ef82c81f
Hithesh B <hitesh99017@gmail.com>
2024-07-05 00:43:05 +0530

    updated NavLink in Layout component

--- src/Rcomponents/Layout.jsx
+++ src/Rcomponents/Layout.jsx
@@ -1,14 +1,39 @@
-import React from 'react';
-import { Outlet, Link } from 'react-router-dom';
+import React from "react";
+import { Outlet, NavLink } from "react-router-dom";
 
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <nav className="bg-blue-600 p-4 text-white">
         <ul className="flex space-x-4">
-          <li><Link to="/" className="hover:underline">Home</Link></li>
-          <li><Link to="/about" className="hover:underline">About</Link></li>
-          <li><Link to="/dashboard" className="hover:underline">Dashboard</Link></li>
+          <li>
+            <NavLink
+              to="/"
+              className={({ isActive }) => 
+                isActive ? "font-bold" : "text-white"
+              }
+
+            // className={( {isActive }) =>
+            //     isActive ? "p-2 bg-cyan-500  text-amber-400 rounded-md" : " text-white"
+            //  }
+            >
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/about" className={({ isActive }) => 
+                isActive ? "font-bold" : "text-white"
+              }>
+              About
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/dashboard"className={({ isActive }) => 
+                isActive ? "font-bold" : "text-white"
+              }>
+              Dashboard
+            </NavLink>
+          </li>
         </ul>
       </nav>
       <div className="flex-grow p-4">
